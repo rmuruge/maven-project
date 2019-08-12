4b92c8f9d51eea07cc2b3e966b2ae2153d59b658
Ubuntu <rmuruge@gmail.com>
2019-08-12 02:48:36 +0000

    changes in Read Me and App java for lesson 3

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,7 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+	System.out.println(new App().getMessage() + "From Lesson 3.0");
     }
 
     private final String getMessage() {
